6377bdbec2da5c5ad365ea3ca7a90a44aad95d44
Akbar Gumbira <akbargumbira@gmail.com>
2014-07-21 17:24:23 +0700

    Some tweaks

--- run.sh
+++ run.sh
@@ -4,12 +4,23 @@
 REALTIME_DIR=/home/realtime
 INASAFE_REALTIME_IMAGE=docker-inasafe-realtime
 
-# Kill previous container
-docker.io kill inasafe-realtime
-docker.io rm inasafe-realtime
+# TODO: Grab the user name and password automatically
+SFTP_LOCAL_IP=$(docker inspect inasafe-realtime-sftp | grep IPAddress | cut -d '"' -f 4)
+SFTP_LOCAL_PORT=22
+SFTP_USER_NAME=realtime
+SFTP_USER_PASSWORD=ieZ4dohn2sab
+SFTP_BASE_PATH=/shakemaps
 
 docker.io run --name="inasafe-realtime" \
--p 2222:22 \
+-e EQ_SFTP_BASE_URL=${SFTP_LOCAL_IP} \
+-e EQ_SFTP_PORT=${SFTP_LOCAL_PORT} \
+-e EQ_SFTP_USER_NAME=${SFTP_USER_NAME} \
+-e EQ_SFTP_USER_PASSWORD=${SFTP_USER_PASSWORD} \
+-e EQ_SFTP_BASE_PATH=${SFTP_BASE_PATH} \
 -v ${REALTIME_DIR}/shakemaps-cache:${REALTIME_DIR}/shakemaps-cache \
 -v ${REALTIME_DIR}/shakemaps-extracted:${REALTIME_DIR}/shakemaps-extracted \
--d -t AIFDR/${INASAFE_REALTIME_IMAGE}:latest
+-i -t AIFDR/${INASAFE_REALTIME_IMAGE}
+
+# Kill the container right away
+docker.io kill inasafe-realtime
+docker.io rm inasafe-realtime
